08b6fc1edafeef9e0a01947c3f9d93b3cd6795c6
FOLUWA <moronfoluwaakintola@gmail.com>
2019-04-25 22:34:16 +0100

    I fixed db connection

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ const router = express.Router();
 //ROUTES
 const routes = require('./routes/index');
 const app = express();
-const port = 3002;
+const port = 3003;
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/blogger", { useNewUrlParser: true}).then(
+mongoose.connect("mongodb://wgbadmin:wgbadmin1@ds227594.mlab.com:27594/the-wgb", { useNewUrlParser: true}).then(
   function(res){
    console.log("Connected to Database Successfully.");
   }
@@ -29,10 +29,10 @@ require('./config/passport');
 
 
 //LOG EVERY REQUEST THAT COMES IN
-// app.use((req,res,next)=>{
-// 	console.log(`${new Date().toString()} => ${req.originalUrl}`);
-// 	next();
-// });
+app.use((req,res,next)=>{
+	console.log(`${new Date().toString()} => ${req.originalUrl}`);
+	next();
+});
 
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
